e4507da78c2fc7336d9ba6bcd191c3685a91c709
Mike Terhar <mterhar@hey.com>
2022-02-01 16:52:06 +0000

    Reverse if logic

--- install.sh
+++ install.sh
@@ -38,6 +38,8 @@ fi
 mkdir -p ~/.vim_backup
 
 if [ -z "$CODER_ASSETS_ROOT" ]; then
+  echo "no coder, no extensions"
+else
   echo "insalling gitlens"
   ${CODER_ASSETS_ROOT}/code-server/bin/code-server --install-extension ${DOTFILES_DIR}/extensions/eamodio.gitlens-11.7.0.vsix
 fi
